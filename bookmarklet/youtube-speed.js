9ab438fe9bcf76a428f4341ff81a8399a4701f0a
sudden.wikholm <sudden.wikholm@gmail.com>
2020-07-14 22:39:33 +0200

    Add youtube playback speed bookmarklet

---
+++ bookmarklet/youtube-speed.js
@@ -0,0 +1,9 @@
+// 1.5x speed
+javascript: (function () {
+  document.getElementsByTagName("video")[0].playbackRate = 1.5;
+})();
+
+// 1x speed
+javascript: (function () {
+  document.getElementsByTagName("video")[0].playbackRate = 1;
+})();
